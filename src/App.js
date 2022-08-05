ec350e49cc49086696805fa6d9c90aeeb580a670
Mohamed Z <mz7891555@gmail.com>
2022-08-05 03:58:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
-import Info from './Info'
-import About from './About';
-import Interests from './Interests';
+import Props from './Props'
 import './File.css'
-import Footer from './Footer'
+import Data from "./Data"
+import Navbar from './Navbar';
 
 
 
 function App(){
+  const myElements = Data.map(e => 
+    <Props 
+    title = {e.title}
+    location = {e.location}
+    maps= {e.googleMapsUrl}
+    startDate = {e.startDate}
+    endDate = {e.endDate}
+    description= {e.description}
+    img= {e.imageUrl}
+    />
+    )
   return(
   <div className='container'>
-    <Info />
-    <About/>
-    <Interests/>
-    <Footer />
+    <Navbar />
+    {myElements}
 
   </div>
   )
---
+++ src/Data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },{
+        title: "Geirangerfjord",
+        location: "NORWAY",
+        googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1049385,6.9351308,11z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://www.hurtigruten.com/globalassets/photos/destinations/norway/attractions/geirangerfjord/geiranger_view.jpg?width=1900&height=950&center=0.51,0.54&transform=DownResizeCrop"
+    }
+    ,{
+        title: "Niagara Falls",
+        location: "USA",
+        googleMapsUrl: "https://www.google.com/maps?q=niagara+falls&source=lmns&hl=en&sa=X&ved=2ahUKEwju0rWL44D5AhXngc4BHfMvCrsQ_AUoAnoECAEQAg",
+        
+        description: "Niagara Falls is a group of three waterfalls at the southern end of Niagara Gorge, spanning the border between the province of Ontario in Canada and the state of New York in the United States.",
+        imageUrl: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcTQ2jHeCNBoziWbGrtre9mSxk4hPcOIl3I_bPySB_xt6pjt3fS5MmhetYGH1x0d1aL2"
+    }
+]
